49e7af98c4eed7fd4dc470e868388adf4d51a427
Fabio Marques <fabiomanuelsilvamarques@gmail.com>
2019-04-11 15:33:28 +0200

    implementation truxs de base

---
+++ ArrayDeque.h
@@ -0,0 +1,41 @@
+#ifndef ArrayDeque_hpp
+#define ArrayDeque_hpp
+
+#include <vector>
+
+template < class T >
+class ArrayDeque
+{
+public:
+   using value_type = T;
+   using reference = T&;
+   using const_reference = const T&;
+   using size_type = std::size_t;
+   using difference_type = std::ptrdiff_t;
+
+private:
+   std::vector<value_type> buffer;
+   size_type debut;
+   size_type taille;
+
+public:
+   ArrayDeque(size_type capacity = 0) : debut(0), taille(0), buffer(capacity)
+   {
+   }
+   size_type size() const;
+   size_type capacity() const;
+   bool empty() const;
+   ArrayDeque<T>& push_back(value_type val);
+   ArrayDeque<T>& push_front(value_type val);
+   reference back() const;
+   reference front() const;
+   value_type pop_front();
+   value_type pop_back();
+
+
+// Completer cette classe générique pour qu'elle passe le codecheck
+
+
+};
+
+#endif /* ArrayDeque_hpp */
