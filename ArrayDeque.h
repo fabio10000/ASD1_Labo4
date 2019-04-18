de49e35d18252eb7a21086758c2dfe4f27d29c4c
Fabio Marques <fabiomanuelsilvamarques@gmail.com>
2019-04-18 16:30:22 +0200

    trucs

--- ArrayDeque.h
+++ ArrayDeque.h
@@ -21,6 +21,11 @@ private:
 public:
    ArrayDeque(size_type capacity = 0) : debut(0), taille(0), buffer(capacity) {
    }
+   ArrayDeque& operator=(const ArrayDeque& rhs) {
+      buffer = rhs.buffer;
+      debut = rhs.debut;
+      taille = rhs.taille;
+   };
 
    size_type size() const {
       return buffer.size();
@@ -29,30 +34,80 @@ public:
       return taille;
    };
    bool empty() const {
-      return debut == 0;
+      return taille == 0;
    };
-   ArrayDeque<T>& push_back(value_type val) {
+   void show() {
+      std::cout << "[";
+      for (auto elem : buffer) {
+         std::cout << elem << ", ";
+      }
+      std::cout << "]" << std::endl;
+   }
+   void newVector() {
+      if (size() == 0) {
+         buffer.resize(1);
+      } else {
+         size_type nb_elems = size() - debut - 1;
+         std::vector<value_type> tmp(size() * 2);
+         tmp.insert(tmp.begin(), buffer.rbegin(), buffer.rend() - debut);
+         tmp.insert(tmp.begin() + nb_elems, buffer.begin(), buffer.begin() + debut);
+         debut = size() - 1;
+         swap(tmp, buffer);
+      }
+   }
+   ArrayDeque& push_back(value_type val) {
+      if (taille == size()) {
+         newVector();
+      }
+      size_type indice = (taille > 0) ? (size() - (taille - debut)) : 0;
+      ++taille;
+      buffer.at(indice) = val;
+   };
+   ArrayDeque& push_front(value_type val) {
+      if (taille == size()) {
+         newVector();
+         show();
+      }
 
+      if( taille != 0) {
+         ++debut;
+      }
+      ++taille;
+      buffer.at(debut) = val;
    };
-   ArrayDeque<T>& push_front(value_type val) {
+   const_reference back() const {
+      return back();
+   }
+   reference back() {
+      size_type indice = (taille > 0) ? (taille - debut - 1) : 0;
+      if (indice != 0) {
+         indice = size() - indice;
+      }
 
+      return buffer.at(indice);
    };
-   reference back() const {
 
+   const_reference front() const {
+      return front();
+   }
+   reference front() {
+      return buffer.at(debut);
    };
-   reference front() const {
+   ArrayDeque& pop_front() {
+      for (size_t i = debut;  i < taille - 1 ; ++i) {
+         buffer.at(i) == buffer.at(i + 1);
+      }
 
+      return *this;
    };
-   void pop_front() {
-      if (debut > 0) {
-         for (size_t i = 0;  i < debut ; ++i) {
+   ArrayDeque& pop_back() {
+      if(debut > 0) {
+         for (size_t i = --debut;  i < taille - 1 ; ++i) {
             buffer.at(i) == buffer.at(i + 1);
          }
-         --debut;
       }
-   };
-   void pop_back() {
-      if (debut > 0) --debut;
+
+      return *this;
    };
 
 
